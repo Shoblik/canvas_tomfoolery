4ee5f571067167a62f928504dff0fcce5283c320
Shoblik <shoblik@yahoo.com>
2018-03-28 17:36:34 -0700

    bouncing completed

--- main.js
+++ main.js
@@ -1,106 +1,74 @@
-var canv = document.getElementById('canvas'),
-    c = canv.getContext('2d');
-canv.width = document.body.clientWidth;
-canv.height = document.body.clientHeight;
-canvW = canv.width;
-canvH = canv.height;
+(function() {
+    var canv = document.getElementById('canvas'),
+        c = canv.getContext('2d'),
+        gravity = .1,
+        dampening = .99;
 
-var x = document.body.clientWidth / 2,
-    y = document.body.clientHeight / 2,
-    radius = 10,
-    incrementor = .001;
+    canv.width = document.body.clientWidth;
+    canv.height = document.body.clientHeight;
+    canvW = canv.width;
+    canvH = canv.height;
 
-document.onkeydown = checkKey;
 
-function checkKey(e) {
-
-    e = e || window.event;
-
-    if (e.keyCode == '38') {
-        // up arrow
-        executeFrame('up');
-
-    }
-    else if (e.keyCode == '40') {
-        // down arrow
-        console.log('downArrow');
-        executeFrame('down');
-    }
-    else if (e.keyCode == '37') {
-        // left arrow
-        executeFrame('left');
-    }
-    else if (e.keyCode == '39') {
-        // right arrow
-        executeFrame('right');
+    var circle = {
+        x: document.body.clientWidth / 2,
+        y: 10,
+        //(vx, vy) = Velocity vector
+        vx: 0,
+        vy: 0,
+        radius: 30,
     }
 
-}
-var speed = 2;
+    function executeFrame() {
+        console.log(circle.vy);
+        circle.x += circle.vx;
+        circle.y += circle.vy;
 
-function executeFrame(direction, bool) {
-    switch(direction) {
-        case 'down':
-            y += speed;
-            break;
-        case 'up':
-            y -= speed;
-            break;
-        case 'left':
-            x -= speed;
-            break;
-        case 'right':
-            x += speed;
-            break;
-    }
+        //Increment Gravity
+        circle.vy += gravity;
 
-//gets rid of the old frame
-    c.clearRect(0,0, canv.width, canv.height);
+        // Slow it down
+        // circle.vy *= dampening;
+        // circle.vx *= dampening;
 
-//Creating the circle
-    for (let i = 0; i < 1; i++) {
+        // Slow it down
+        if (Math.floor(circle.vy) === -1) {
+            circle.vy = 0;
+        } else if ((circle.vy) < -1) {
+            console.log((circle.vy));
+            circle.vy += gravity;
+        }
+
+        //Bounce
+        if (circle.y + circle.radius > canv.height) {
+            //check to see if the ball is at the bottom
+            // circle.y = canv.height - circle;
+            circle.vy = -Math.abs(circle.vy);
+        }
+
+        //gets rid of the old frame
+        c.clearRect(0,0, canv.width, canv.height);
+
+        //Creating the circle
         c.beginPath();
-        c.arc(x + (20 * i), y, radius, 0, 2 * Math.PI);
+        c.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI);
         c.fillStyle = '#FF3200';
         c.fill();
         c.closePath();
+
+        requestAnimFrame(executeFrame);
+
+        // if (circle.y < canv.height - circle.radius) {
+        //     //if we want to keep the animation going tell call it again
+        // } else {
+        //     //if you want the animation to end or change
+        //
+        // }
     }
-
-
-    if (y < canv.height - radius) {
-        //if we want to keep the animation going tell call it again
-        // requestAnimFrame(executeDownFrame);
-    } else {
-        //if you want the animation to end or change
-        console.log('end');
-        // requestAnimFrame(executeUpFrame);
-
-    }
-}
-function executeUpFrame() {
-    y = y-- + (incrementor * -1);
-    incrementor += .01;
-
-//gets rid of the old frame
-    c.clearRect(0,0, canv.width, canv.height);
-
-    for (let i = 0; i < Math.floor(document.body.clientWidth / radius); i++) {
-        c.beginPath();
-        c.arc(x + (20 * i), y, radius, 0, 2 * Math.PI);
-        c.fillStyle = '#FF3200';
-        c.fill();
-        c.closePath();
-    }
-
-
-    if (y > radius) {
-        requestAnimFrame(executeUpFrame);
-    } else {
-        requestAnimFrame(executeDownFrame);
-    }
-}
 
 
 //Start animation
-executeFrame();
+    executeFrame();
 
+
+})();
